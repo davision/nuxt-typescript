84799975802205cc6f3a329ee2782502e13b6b6d
davidlicen <david@artnetik.si>
2018-10-03 17:52:43 +0200

    Resolve #1 issue

--- store/index.ts
+++ store/index.ts
@@ -1,18 +1,15 @@
 export const state = () => ({
-  people: []
+  people: [
+    {"id":1,"first_name":"Sebastien","last_name":"Thorsby","contact":{"email":"sthorsby0@psu.edu","phone":"1-(208)492-2543"},"gender":"Male","ip_address":"250.213.72.156","avatar":"https://robohash.org/quimodisint.png?size=200x200&set=set1","address":{"city":"Idaho Falls","country":"United States","postalCode":"83405","state":"ID","street":"9465 Utah Crossing"}},
+    {"id":2,"first_name":"Neville","last_name":"Chastey","contact":{"email":"nchastey1@fema.gov","phone":"1-(205)900-8038"},"gender":"Male","ip_address":"150.45.99.76","avatar":"https://robohash.org/ducimusenimiste.bmp?size=200x200&set=set1","address":{"city":"Birmingham","country":"United States","postalCode":"35279","state":"AL","street":"38347 Springview Avenue"}},
+    {"id":3,"first_name":"Nanny","last_name":"Humble","contact":{"email":"nhumble2@ft.com","phone":"1-(920)406-2490"},"gender":"Female","ip_address":"17.255.88.28","avatar":"https://robohash.org/asperioresnesciuntqui.png?size=200x200&set=set1","address":{"city":"Green Bay","country":"United States","postalCode":"54305","state":"WI","street":"2126 Towne Center"}}
+  ]
 })
 
 export const mutations = {
-  setPeople(state, people) {
-    state.people = people
-  }
+
 }
 
 export const actions = {
-  async nuxtServerInit({ commit }, { app }) {
-    const people = await app.$axios.$get(
-      "./random-data.json"
-    )
-    commit("setPeople", people.slice(0, 10))
-  }
+
 }
